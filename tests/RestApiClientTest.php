d1226b7f7ee19c3108297ac836f8d2e524843d45
Wellming Li <wellming.li@howzhi.com>
2016-08-16 02:16:42 +0800

    mock http request

--- tests/RestApiClientTest.php
+++ tests/RestApiClientTest.php
@@ -2,6 +2,7 @@
 
 use Codeages\RestApiClient\RestApiClient;
 use Codeages\RestApiClient\Specification\JsonHmacSpecification;
+use Codeages\RestApiClient\HttpRequest\MockHttpRequest;
 
 class RestApiClientTest extends \PHPUnit_Framework_TestCase
 {
@@ -14,23 +15,16 @@ class RestApiClientTest extends \PHPUnit_Framework_TestCase
             'endpoint' => 'http://domain.tld/api/v1/',
         );
         $spec = new JsonHmacSpecification();
+        $http = new MockHttpRequest([]);
 
-        $client = new RestApiClient($config, $spec);
+        $client = new RestApiClient($config, $spec, $http);
 
+        $http->mock(function() {
+            return 1;
+        });
         $result = $client->get('/');
 
-        var_dump($result);
-
-
-        // $service = new ApiService([
-        //     'accessKey' => 'N3fSA9br6d4Ko9fbwuhZNmjkxElgEAlr',
-        //     'secretKey' => 'DAkdKwFQJKB8TQjqlAp8XJtuoOL3p6u2',
-        //     'endpoint' => 'root-api.dev.com/v1'
-        // ]);
-
-        // $me = $service->getMe();
-
-        // var_dump($me);
+        $this->assertEquals(1, $result);
     }
 
 }
